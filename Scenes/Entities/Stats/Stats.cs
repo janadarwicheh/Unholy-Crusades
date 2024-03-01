f2ddab748d1e3a5a06b0bf75a188dc9b30ed3994
Charles <charleshalmov13@gmail.com>
2024-03-01 21:50:54 +0100

    1

---
+++ Scenes/Entities/Stats/Stats.cs
@@ -0,0 +1,57 @@
+﻿namespace Skull.Scenes.Entities.Stats;
+
+public class Stats
+{
+    protected int amount ;
+
+    public void Change(int amount)
+    {
+        
+    }
+}
+
+public class HP : Stats
+{
+    public void Death()
+    {
+        
+    }
+}
+
+public class Hit_Points : Stats 
+{
+
+}
+
+public class Armor : Stats
+{
+    
+}
+
+public class Speed : Stats
+{
+    
+}
+
+public abstract class Capacities
+{
+    
+    protected int Damage_Amount { get; set; }
+    
+    protected int Heal_Amount { get; set; } 
+    
+    protected int[] rng { get; set; }
+    
+    protected int  range { get;  set  ; }
+    
+    protected int cooldown { get; set; }
+    
+    protected abstract void launch();
+
+}
+
+
+public abstract class Spells : Capacities 
+{
+    
+}
