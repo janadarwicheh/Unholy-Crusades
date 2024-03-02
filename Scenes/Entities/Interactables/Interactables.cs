9f658a10f0cb96e29ba82a9537980219c52f4d51
Lmao ZeDodongo <maximely206@gmail.com>
2024-03-02 09:07:56 +0100

    Composantes d'entités, avec inventaire et liste de Stats, classes de ressources et objets intéragissable. Map très légèrement modifiée. à faire : les coffres, les caractéristiques des personnages, finaliser les classes et composants, terminer la map. Maxime.

---
+++ Scenes/Entities/Interactables/Interactables.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Skull.Scenes.Entities.e.Resources;
+
+namespace Skull.Scenes.Entities.Interactables;
+
+public abstract class Interactables
+{
+    public abstract void Interact();
+    public Dictionary<Resource, bool> Givables;
+    
+
+    protected Interactables(Dictionary<Resource, bool> givables)
+    {
+        Givables = givables;
+    }
+}
