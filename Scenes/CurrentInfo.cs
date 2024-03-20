2411d259f4e25d3c106d1ef9b4f1a7406ee228d8
Lmao ZeDodongo <maximely206@gmail.com>
2024-03-20 14:23:59 +0100

    Vitesse et IA fixées

--- Scenes/CurrentInfo.cs
+++ Scenes/CurrentInfo.cs
@@ -7,6 +7,7 @@ public partial class CurrentInfo : Node
 {
     public static World CurrentScene;
     public static Playeru player;
+    public static float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle() * 20;
     public override void _Ready()
     {
         CurrentScene = (World)GetTree().CurrentScene;
