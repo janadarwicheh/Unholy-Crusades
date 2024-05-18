335049fa5275199bfd4ea994339337963e3a0aff
Lmao ZeDodongo <maximely206@gmail.com>
2024-05-18 19:41:52 +0200

    MPMP

--- Scenes/Multiplayer/MultiplayerController.cs
+++ Scenes/Multiplayer/MultiplayerController.cs
@@ -1,16 +1,17 @@
 using Godot;
 using System;
 
+public partial class MultiplayerController : Control 
 
-public partial class MultiplayerController : Control
 {
 	[Export]
 	private int port = 8910;
-
-	[Export]
+	
+    [Export]
 	private string address = "127.0.0.1";
 
 	private ENetMultiplayerPeer peer;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -20,19 +21,6 @@ public partial class MultiplayerController : Control
 		Multiplayer.ConnectionFailed += ConnectionFailed;
 	}
 
-	//Runs when a Player connects and runs on all peers
-	private void PeerConnected(long id)
-	{
-		GD.Print("Player Connected! " + id.ToString());
-	}
-
-	//Runs when a Player disconnect and runs on all peers
-	private void PeerDisconnected(long id)
-	{
-		GD.Print("Player Disconnected: " + id.ToString());
-	}
-
-	//Runs when the connection failed and only runs on the clients
 	private void ConnectionFailed()
 	{
 		GD.Print("CONNECTION FAILED");
@@ -40,52 +28,67 @@ public partial class MultiplayerController : Control
 
 	private void ConnectedToServer()
 	{
-		GD.Print("Connected To Server");
+		GD.Print("CONNECTED TO SERVER");
+	}
+
+	private void PeerDisconnected(long id)
+	{
+		GD.Print("PLAYER DISCONNECTED: " + id.ToString());
+	}
+
+	private void PeerConnected(long id)
+	{
+		GD.Print("PLAYER CONNECTED! " + id.ToString());
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
 	}
 	
-	public override void _Process(double delta){}
-
-
 	public void _on_host_button_down()
 	{
 		peer = new ENetMultiplayerPeer();
 		var error = peer.CreateServer(port, 2);
-		if(error != Error.Ok){
-			GD.Print("error cannot host! :" + error.ToString());
+		
+		if (error != Error.Ok)
+		{
+			GD.Print("ERROR CANNOT HOST: " + error.ToString());
 			return;
 		}
+		
 		peer.Host.Compress(ENetConnection.CompressionMode.RangeCoder);
 
 		Multiplayer.MultiplayerPeer = peer;
-		GD.Print("Waiting For Players!");
+		GD.Print("WAITING FOR PLAYERS!");
 	}
 
 	public void _on_join_button_down()
 	{
 		peer = new ENetMultiplayerPeer();
 		peer.CreateClient(address, port);
-
+		
 		peer.Host.Compress(ENetConnection.CompressionMode.RangeCoder);
 		Multiplayer.MultiplayerPeer = peer;
-		GD.Print("Joining Game!");
+		GD.Print("JOINING GAME...");
 	}
 
-
-	public void _on_start_game_button_down(){
-		Rpc("startGame");
+	public void _on_start_game_button_down()
+	{
+		Rpc("StartGame");
 	}
 
 	[Rpc(MultiplayerApi.RpcMode.AnyPeer, CallLocal = true, TransferMode = MultiplayerPeer.TransferModeEnum.Reliable)]
-	private void startGame(){
-
-		var scene = ResourceLoader.Load<PackedScene>("res://world.tscn").Instantiate<Node>();
+	
+	private void StartGame()
+	{
+		var scene = ResourceLoader.Load<PackedScene>("res://Scenes/Map/world1.tscn").Instantiate();
 		GetTree().Root.AddChild(scene);
 		this.Hide();
 	}
-	
-	private void sendPlayerInformation(string name,int id)
+
+	private void SendPlayerInformation(string name, int id)
 	{
-		
+		throw new NotImplementedException();
 	}
-	
 }
