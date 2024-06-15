7070fb50f8e275b911e2223688aaa67dde7e74e7
ZeDodongo <maximely206@gmail.com>
2024-06-16 00:18:27 +0200

    qzdqzdsdqdz

--- Scenes/Entities/Boss.cs
+++ Scenes/Entities/Boss.cs
@@ -2,7 +2,7 @@ using Godot;
 using System;
 using Skull.Scenes.Entities.Parameters;
 
-public partial class Boss : Enemy
+public partial class Boss : enemy
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
